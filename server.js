6bcbf47147450d2c5dffcd146c991e5b07c4049a
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-04-27 17:30:04 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-// Needs changes to be made ready for Heroku. See end of video,
-// or previous code used to deploy.
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -18,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
